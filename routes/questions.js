78e9a17d51c2848f967f3a06c406536aabb3aec2
maximerchrd <maximerchrd@gmail.com>
2018-05-10 14:44:14 +0400

    added column to show if question is selected

--- routes/questions.js
+++ routes/questions.js
@@ -3,16 +3,18 @@ var router = express.Router();
 const mysql = require('mysql');
 
 //define question object
-function Question(questionText, questionType, imageName, rating) {
+function Question(questionText, questionType, imageName, rating, userSelected) {
     this.questionText = questionText;
     this.questionType = questionType;
     this.imageName = imageName;
     this.rating = rating;
+    this.userSelected = userSelected
 }
 
 
 /* GET questions page. */
 router.get('/', function(req, res, next) {
+    var questSelectedFilter = ["All questions", "My questions", "Other questions"];
     var mainSubjects = [];
     var questionsArray = [];
 
@@ -38,16 +40,26 @@ router.get('/', function(req, res, next) {
         con.query("SELECT * FROM short_answer_questions;", function (err, rows) {
             if (err) throw err;
             for (var i in rows) {
-                var question = new Question(rows[i].QUESTION, "Short Answer", rows[i].IMAGE_PATH, 3);
-                questionsArray.push(question);
+                if (req.user) {
+                    var question = new Question(rows[i].QUESTION, "Short Answer", rows[i].IMAGE_PATH, 3, "selected.png");
+                    questionsArray.push(question);
+                } else {
+                    var question = new Question(rows[i].QUESTION, "Short Answer", rows[i].IMAGE_PATH, 3, "notselected.png");
+                    questionsArray.push(question);
+                }
             }
 
         });
         con.query("SELECT * FROM multiple_choice_questions;", function (err, rows) {
             if (err) throw err;
             for (var i in rows) {
-                var question = new Question(rows[i].QUESTION, "Multiple Choice", rows[i].IMAGE_PATH, 3);
-                questionsArray.push(question);
+                if (req.user) {
+                    var question = new Question(rows[i].QUESTION, "Multiple Choice", rows[i].IMAGE_PATH, 3, "selected.png");
+                    questionsArray.push(question);
+                } else {
+                    var question = new Question(rows[i].QUESTION, "Multiple Choice", rows[i].IMAGE_PATH, 3, "notselected.png");
+                    questionsArray.push(question);
+                }
             }
 
             var signString = ""
@@ -62,7 +74,7 @@ router.get('/', function(req, res, next) {
 
             var data = {questions: questionsArray};
             res.render('questions', { sign_in_out: signString, sign_in_out_url: signUrl, data: data,
-                mainSubjects: mainSubjects});
+                mainSubjects: mainSubjects, questSelectedFilter: questSelectedFilter});
         });
     });
 });
